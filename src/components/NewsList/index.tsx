f4c45aa7482d36b4a3d2b56b94ae967ec1e4ded7
Elizeu Louzeiro <elizeumatheus16@gmail.com>
2021-01-25 12:56:03 -0300

    feat: created component to handle with card list

---
+++ src/components/NewsList/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+import styledMedia from "src/styles/styledMedia"
+
+const NewsList = styled.section`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr;
+
+  ${styledMedia.greaterThan("tablet")`
+    gap: 1.25rem;
+    grid-template-columns: repeat(2, 1fr);
+  `}
+
+  ${styledMedia.greaterThan("laptopL")`
+    gap: 1.5rem;
+    grid-template-columns: repeat(3, 1fr);
+  `}
+`
+
+export { NewsList }
