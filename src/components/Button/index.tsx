55e097cfb7c041b4caa8286f277918e42b076e99
Elizeu Louzeiro <elizeumatheus16@gmail.com>
2021-01-25 12:55:34 -0300

    feat: created button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  background-color: #7097fa;
+  border-radius: 8px;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  font-weight: bold;
+  outline: none;
+  padding: 0.75rem 1rem;
+
+  &:hover {
+    background-color: #5d8af9;
+  }
+`
+
+export { Button }
